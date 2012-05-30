93413f49d0287c413ad6c5fc47e7400137f838ac
Kelly Banman <kelly.banman@gmail.com>
2012-05-30 12:34:15 -0700

    Enhanced functionality

--- hmvc.php
+++ hmvc.php
@@ -1,14 +1,32 @@
-<?php namespace HMVC;
+<?php namespace HMVC; use \URI, \Router;
 
 class HMVC {
 	
+	protected static $routes = array();
+
+	public static $main_uri;
+
 	public static function __callStatic($method, $arguments)
 	{
-		$uri = $arguments[0];
+		if ( ! empty(static::$routes) && isset(static::route()->$method))
+		{
+			return static::route()->$method;
+		}
+
+		return static::call_hmvc($method, $arguments);
+	}
+
+	public static function call_hmvc($method, $arguments)
+	{
+		array_unshift($arguments, 'test/uri');
+		if ( ! $uri = Router::uses($arguments[1]))
+		{
+			$uri = str_replace(array('::', '@'), '/', $arguments[1]);
+		}
 		$action = array('uses' => $arguments[1]);
 		$parameters = isset($arguments[2]) ? $arguments[2] : array();
 
-		$route = new \Route(strtoupper($method), $uri, $action, $parameters);
+		$route = static::$routes[] = new \Route(strtoupper($method), $uri, $action, $parameters);
 
 		// Inject false input data
 		if ( ! empty($arguments[3]))
@@ -16,6 +34,29 @@ class HMVC {
 			\Input::merge($arguments[3]);
 		}
 
-		return $route->call();
+		// Temporarily mess with URI::current()
+		$old_uri = URI::$uri;
+		URI::$uri = $uri;
+		(count(static::$routes) == 1) && static::$main_uri = URI::$uri;
+
+		$result = $route->call();
+
+		// Put things back in place
+		URI::$uri = $old_uri;
+
+		array_pop(static::$routes);
+		count(static::$routes) || static::$main_uri = null;
+
+		return $result;
+	}
+
+	public static function route()
+	{
+		return end(static::$routes);
+	}
+
+	public static function active()
+	{
+		return empty(static::$routes);
 	}
 }
